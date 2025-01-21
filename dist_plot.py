e41b3e3652a2a99952dece1c0baba37957543dd7
bcdemattei <bcdemattei@gmail.com>
2025-01-21 21:24:43 +0000

    updated dist_plot

--- dist_plot.py
+++ dist_plot.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pathlib as Path
 
-df = pd.read_csv("Time1553_distances.csv")
+def process_and_plot_distances(file_path, source):
+
+    p = Path(source)
+
+    df = pd.read_csv(file_path)
+    df.drop(df.columns[[0]], axis = 1, inplace=True)
+    df = df.to_numpy()
+    df = df.transpose()
 
 
-img = plt.hist(df)
-plt.imsave("test.png", img)
+    img = plt.hist(df[~np.isnan(df)], bins = 20)
+    plt.xlabel("Normalized Distance of C. erosa to Bdelloid")
+    plt.ylabel("Frequency of Distances Across All Frames")
+    plt.title(f"{p.stem} Distances")
+    plt.show(img)
+
+    print(f"Max normalized distance: {np.nanmax(df)}")
+    print(f"Min normalized distance: {np.nanmin(df)}")
